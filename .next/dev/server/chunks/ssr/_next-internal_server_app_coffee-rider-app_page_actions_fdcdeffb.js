5d4b04d86e874816e5d624ef1f36348165cb6320
Raymond Sugiarto <raymondsugiarto@gmail.com>
2026-02-03 22:35:42 +0700

    Remove .next directory from tracking

--- .next/dev/server/chunks/ssr/_next-internal_server_app_coffee-rider-app_page_actions_fdcdeffb.js
+++
@@ -1,9 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/coffee-rider-app/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__) => {
-"use strict";
-
-__turbopack_context__.s([]);
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_coffee-rider-app_page_actions_fdcdeffb.js.map
--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"qGsSM4aOdCrU9YpFJASAPo2YrlYY1Gu2I9tD4G9uRAk=\"\n}"
