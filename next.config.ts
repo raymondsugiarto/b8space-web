9ba381343c701068f9ba12806dab333c49ece5e0
Raymond Sugiarto <raymondsugiarto@gmail.com>
2026-02-03 23:11:49 +0700

    error output dist

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,10 @@ import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
   trailingSlash: true,
   reactStrictMode: true,
   images: {
     domains: [],
-    unoptimized: true, // Required for static export
   },
 };
 
